781db9f48474e8f917112f000bf1d4f1dbe1d8f1
Denis Yalymov <>
2023-03-25 16:01:32 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Первое число: ");
+int numOne = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Второе число: ");
+int numTwo = Convert.ToInt32(Console.ReadLine());
+
+if (numOne > numTwo)
+{
+    Console.WriteLine("Max = " + numOne + " min = " + numTwo);
+}
+else
+{
+    Console.WriteLine("Max " + numTwo + " min " + numOne);
+}
